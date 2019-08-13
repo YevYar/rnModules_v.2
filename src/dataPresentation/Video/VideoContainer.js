853006160c9a7d132e7f1ace50c33d077043ec01
Євгеній Яремченко <eugeneYarem@gmail.com>
2019-08-13 15:27:26 +0300

    Image gallery was implemented via rn-image-picker

---
+++ src/dataPresentation/Video/VideoContainer.js
@@ -0,0 +1,26 @@
+/**
+ * This module contains container that gives data and functions to VideoScreen.
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Actions } from 'react-native-router-flux';
+
+import VideoScreen from './VideoScreen';
+
+const onImagePress = images => i =>
+  Actions.imagesView({
+    images,
+    imageIndex: i
+  });
+
+const bufferConfig = {
+  minBufferMs: 15000,
+  maxBufferMs: 50000,
+  bufferForPlaybackMs: 5500,
+  bufferForPlaybackAfterRebufferMs: 3000
+};
+
+/* Screen requires bufferConfig, theme */
+export default props => <VideoScreen {...props} bufferConfig={bufferConfig} />;
